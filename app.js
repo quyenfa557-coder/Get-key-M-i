351c7986d85c89f6f5aa2cd7839add7c6920d2f1
quyenfa557-coder <quyenfa557@gmail.com>
2026-09-09 10:59:22 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ function showKey(data) {
 
 function setGenerateButtonLoading(
   loading,
-  text = "Nhận key ngay"
+  text = "Generate"
 ) {
   const button = $("generateBtn");
 
@@ -94,7 +94,7 @@ function setGenerateButtonLoading(
 
   button.innerHTML = loading
     ? `<span>⌛</span><span>${text}</span>`
-    : "<span>Nhận key ngay</span><span aria-hidden=\"true\">→</span>";
+    : "<span>⇥</span><span>Generate</span>";
 }
 
 async function startLink4m() {
